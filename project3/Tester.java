cccbecf5153c189a1e7c259a1e65653309a90774
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-06-27 17:36:37 +0530

    day 2 last cmt,

--- project3/Tester.java
+++ project3/Tester.java
@@ -2,50 +2,65 @@ package project3;
 
 import java.util.Scanner;
 
-import project3.model.Employee;
+// import project3.model.Employee;
 import project3.model.StreamedStudent;
-import project3.model.TaxCalculator;
-import project3.model.Manager;
-import project3.model.RegularEmployee;
+import project3.model.Student;
+// import project3.model.TaxCalculator;
+// import project3.model.Manager;
+// import project3.model.RegularEmployee;
 
 public class Tester {
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+    // Scanner input = new Scanner(System.in);
 
-    StreamedStudent student = new StreamedStudent();
-    System.out.println("Enter student ID:");
-    student.setId(input.nextInt());
-    System.out.println("Enter student name:");
-    student.setName(input.next());
-    System.out.println("Enter student stream:");
-    student.setStream(input.next());
+    // StreamedStudent student = new StreamedStudent();
+    // System.out.println("Enter student ID:");
+    // student.setId(input.nextInt());
+    // System.out.println("Enter student name:");
+    // student.setName(input.next());
+    // System.out.println("Enter student stream:");
+    // student.setStream(input.next());
     
-    RegularEmployee employee = new RegularEmployee();
-    System.out.println("Enter employee ID:");
-    employee.setId(input.nextInt());
-    System.out.println("Enter employee name:");
-    employee.setName(input.next());
-    System.out.println("Enter employee salary:");
-    employee.setSalary(input.nextInt());
+    // RegularEmployee employee = new RegularEmployee();
+    // System.out.println("Enter employee ID:");
+    // employee.setId(input.nextInt());
+    // System.out.println("Enter employee name:");
+    // employee.setName(input.next());
+    // System.out.println("Enter employee salary:");
+    // employee.setSalary(input.nextInt());
 
-    System.out.println("Student details:");
-    student.getDetails();
-    System.out.println("Employee details:");
-    employee.getDetails();
+    // System.out.println("Student details:");
+    // student.getDetails();
+    // System.out.println("Employee details:");
+    // employee.getDetails();
 
-    Manager e = new Manager();
-    e.setId(1);
-    e.setName("John");
-    e.getDetails();
+    // Manager e = new Manager();
+    // e.setId(1);
+    // e.setName("John");
+    // e.getDetails();
   
-    Employee e2 = new RegularEmployee();
-    e2.setId(3);
-    e2.setName("Jane");
-    e2.getDetails();
+    // Employee e2 = new RegularEmployee();
+    // e2.setId(3);
+    // e2.setName("Jane");
+    // e2.getDetails();
 
-    TaxCalculator taxCalculator = new TaxCalculator();
-    taxCalculator.calculateTax(e2);
+    // TaxCalculator taxCalculator = new TaxCalculator();
+    // taxCalculator.calculateTax(e2);
 
-    input.close();
+    // input.close();
+
+    Student s2 = new StreamedStudent();
+    s2.setId(1);
+    s2.setName("John");
+    s2.getDetails();
+    
+    StreamedStudent s3 = (StreamedStudent) s2;
+    s3.setStream("Maths", "Science", "English");
+    s3.getDetails();
+
+    Student[] students = s3.createStudents("John", "Jane", "Jack");
+    for (Student s : students) {
+      s.getDetails();
+    }
   }
 }
--- project3/model/StreamedStudent.java
+++ project3/model/StreamedStudent.java
@@ -1,27 +1,38 @@
 package project3.model;
 
 public class StreamedStudent extends Student {
-  private String stream;
+  private String[] stream;
 
   public StreamedStudent() {
   }
 
-  public String getStream() {
+  public void setStream(String... subjects) {
+    this.stream = subjects.clone();
+  }
+
+  public String[] getStream() {
     return stream;
   }
 
-  public void setStream(String stream) {
-    this.stream = stream;
+  public Student[] createStudents(String... names) {
+    Student[] students = new Student[names.length];
+    for (int i = 0; i < names.length; i++) {
+      students[i] = new Student();
+      students[i].setId(i + 1);
+      students[i].setName(names[i]);
+    }
+    return students;
   }
 
   @Override
   public void getDetails() {
     super.getDetails();
-    System.out.println("Stream: " + stream);
+    if (stream != null && stream.length > 0) System.out.println("Stream: " + String.join(", ", stream));
+    else System.out.println("Stream: None");
   }
 
   @Override
   public String toString() {
-    return super.toString() + "StreamedStudent [stream=" + stream + "]";
+    return super.toString() + "StreamedStudent [stream=" + String.join(", ", stream) + "]";
   }
 }
