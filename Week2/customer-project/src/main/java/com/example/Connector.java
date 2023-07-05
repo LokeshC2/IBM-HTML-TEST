8ae93031e803847d007c23efae54ed5871484095
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-07-05 10:28:48 +0530

    fixing 1

---
+++ Week2/customer-project/src/main/java/com/example/Connector.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.cj.jdbc.Driver;
+
+public class Connector {
+
+  private static Connection connection;
+
+  private static Connection newConnection() throws SQLException {
+    DriverManager.registerDriver(new Driver());
+    connection =
+      DriverManager.getConnection(
+        "jdbc:mysql://localhost:3306/shop",
+        "lox",
+        "mysql0"
+      );
+    return connection;
+  }
+
+  public static Connection getConnection() {
+    if (connection == null) {
+      try {
+        connection = newConnection();
+      } catch (SQLException e) {
+        System.out.println("Error connecting to database: \n" + e.getMessage());
+      }
+    }
+    return connection;
+  }
+}
