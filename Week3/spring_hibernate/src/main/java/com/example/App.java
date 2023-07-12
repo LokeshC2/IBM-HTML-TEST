99ed82a36b06cd7633e11aee392a4315fe86136a
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-07-12 15:52:49 +0530

    prodcut example

--- Week3/spring_hibernate/src/main/java/com/example/App.java
+++ Week3/spring_hibernate/src/main/java/com/example/App.java
@@ -1,13 +1,29 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.example.config.SpringConfig;
+import com.example.entity.Product;
+import com.example.service.ProductService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+
+  public static void main(String[] args) {
+    ApplicationContext context;
+    try {
+      context = new AnnotationConfigApplicationContext(
+        SpringConfig.class
+      );
+      ProductService productService = context.getBean(
+        "productService",
+        ProductService.class
+      );
+      Product product = productService.createProduct("Laptop", 1000, 10);
+      System.out.println(product);
+      productService.saveProduct(product);
+      System.out.println(productService.listProducts());
+    } catch (Exception e) {
+      System.out.println(e);
     }
+  }
 }
