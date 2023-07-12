78b85dc4e057f3558e3c0bc6cf941afe6e8338af
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-07-12 16:37:41 +0530

    Create. list, findbyId, update, delete

--- Week3/spring_hibernate/src/main/java/com/example/App.java
+++ Week3/spring_hibernate/src/main/java/com/example/App.java
@@ -2,6 +2,7 @@ package com.example;
 
 import com.example.config.SpringConfig;
 import com.example.entity.Product;
+import com.example.service.CarService;
 import com.example.service.ProductService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,11 +10,32 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
 
   public static void main(String[] args) {
+    // ProductExample(args);
+    CarExample(args);
+  }
+
+  public static void CarExample(String[] args) {
     ApplicationContext context;
     try {
-      context = new AnnotationConfigApplicationContext(
-        SpringConfig.class
-      );
+      context = new AnnotationConfigApplicationContext(SpringConfig.class);
+      CarService carService = context.getBean("carService", CarService.class);
+      carService.createCar("BMW", "Black");
+      carService.createCar("Audi", "White");
+      carService.createCar("Mercedes", "Red");
+      carService.listCars();
+      carService.findCarById(2);
+      carService.updateCar(2, "Audi", "OceanBlue");
+      carService.deleteCar(3);
+      carService.listCars();
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  public static void ProductExample(String[] args) {
+    ApplicationContext context;
+    try {
+      context = new AnnotationConfigApplicationContext(SpringConfig.class);
       ProductService productService = context.getBean(
         "productService",
         ProductService.class
