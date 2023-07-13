00967122691eace304c9e5548bda9b7cdf0d6971
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-07-13 14:48:20 +0530

    servlet

---
+++ Week3/web-example/src/main/java/com/example/model/Student.java
@@ -0,0 +1,17 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+  private String name;
+  private int age;
+}
