f7bb6de102d6020bd22a7f25b27931e7f7770cc3
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-08-07 16:49:33 +0530

    bend+swggr

--- Week6/department-service/src/main/java/com/example/departmentservice/DepartmentRepository.java
+++ Week6/department-service/src/main/java/com/example/departmentservice/DepartmentRepository.java
@@ -1,6 +1,11 @@
 package com.example.departmentservice;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+  @Query("SELECT d FROM Department d WHERE d.departmentName = ?1")
+  Optional<Department> findByDepartmentName(String departmentName);
 }
--- Week6/department-service/src/main/java/com/example/departmentservice/SwaggerConfig.java
+++ Week6/department-service/src/main/java/com/example/departmentservice/SwaggerConfig.java
@@ -1,18 +1,18 @@
-// package com.example.departmentservice;
+package com.example.departmentservice;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
-// import springfox.documentation.spi.DocumentationType;
-// import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
-// import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.spring.web.plugins.Docket;
 
-// @Configuration
-// @Import(SpringDataRestConfiguration.class)
-// public class SwaggerConfig {
-//   @Bean
-//   Docket swaggerConfiguration() {
-//     return new Docket(DocumentationType.SWAGGER_2);
-//   }
-// }
+@Configuration
+@Import(SpringDataRestConfiguration.class)
+public class SwaggerConfig {
+  @Bean
+  Docket swaggerConfiguration() {
+    return new Docket(DocumentationType.SWAGGER_2);
+  }
+}
